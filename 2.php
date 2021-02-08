916447aa35319ee3cb86a525dea3948aff22c49b
FeoktistovArtyom <feoktistovartyom97@hotmail.com>
2021-02-08 14:13:04 +0500

    created a function convertString

--- 2.php
+++ 2.php
@@ -1,3 +1,4 @@
+<pre>
 <?php
 
 /* 2.2. Задание
@@ -70,3 +71,38 @@
 </Товар>
 </Товары>
 */
+
+/**
+ * Функция для поиска второй подстроки и инверсии её
+ * @param string $a
+ * @param string $b подстрока которую нужно найти в $a
+ * @return string инверсия второй подстроки
+ */
+function convertString(string $a, string $b): string
+{
+    $arr = explode(' ', $a);
+    $stringLowerCase = mb_strtolower($b);
+    $counter = 0;
+
+    foreach($arr as $key => $word) {
+        $wordLowerCase = mb_strtolower($word);
+        $wordTrim = trim($wordLowerCase, "\x20..\x2F\x3A..\x3F");
+
+        if ($stringLowerCase === $wordTrim) $counter++;
+        if ($counter == 2) {
+            $wordArray = preg_split('/(?<!^)(?!$)/u', $word);
+            $wordArrayRev = array_reverse($wordArray);
+            $arr[$key] = implode($wordArrayRev);
+            break;
+        }
+    }
+
+    return implode(' ', $arr);
+}
+
+// var_dump(convertString('меня зовут Меня Артём. У Меня есть кошка, а также у меня есть Пёс', 'меня'));
+
+
+
+?>
+</pre>
